8d9ed300b1cbbc833adcf2bc02305a2c5e0401b5
Hamza hanfi <hanfihamza20@gmail.com>
2022-04-18 05:00:03 +0000

    adding product screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Col, Row, Image, ListGroup } from "react-bootstrap";
+import { Col, Row, Image, ListGroup, Button } from "react-bootstrap";
 import products from "../products";
 import { useParams } from "react-router-dom";
+import Raiting from "../comonents /Raiting";
 
 const ProductScreen = () => {
   const { _id } = useParams();
@@ -24,9 +25,47 @@ const ProductScreen = () => {
           />
         </Col>
         <Col md={3}>
-          <ListGroup variant="flush"></ListGroup>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Raiting
+                value={product.rating}
+                text={`from ${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Price: $ <strong>{product.price}</strong>
+            </ListGroup.Item>
+            <ListGroup.Item>description:{product.description}</ListGroup.Item>
+          </ListGroup>
         </Col>
-        <Col md={3}></Col>
+        <Col md={3}>
+          <ListGroup>
+            <ListGroup.Item>
+              <Row>
+                <Col>price</Col>
+                <Col>{product.price}</Col>
+              </Row>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <Row>
+                <Col>Status</Col>
+                <Col>
+                  <strong>
+                    {product.countInStock ? "In Stock" : "Out of Stock"}
+                  </strong>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+
+            <Button className="btn-block" type="button">
+              Add To Chart
+            </Button>
+          </ListGroup>
+        </Col>
       </Row>
     </>
   );
