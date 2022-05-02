b642430f9e236bc8d20bd8b77e34de4cee08d81b
Hamza hanfi <hanfihamza20@gmail.com>
2022-05-02 15:50:03 +0000

    adding users

---
+++ backend/Data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+  {
+    name: "HANFI Hamza",
+    email: "hamza@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "HANFI youness",
+    email: "adnane@example.com",
+    password: bcrypt.hashSync("12345", 10),
+  },
+  {
+    name: "HANFI Adnane",
+    email: "adnane@example.com",
+    password: bcrypt.hashSync("12456", 10),
+  },
+];
+
+export default users;
